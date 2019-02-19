4234a32c68eba909f0815d47660cdc88866f30a9
Patrick <patrickwharton1@gmail.com>
2019-02-19 19:06:24 +0000

    Basic Errors

--- application.py
+++ application.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/register")
+def register():
+    return render_template("error.html", message="TODO", code="404")
+
 if __name__=="__main__":
         app.run(debug=True)
