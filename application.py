c1ad9fa41af5f92c00ce576530e4e1333482a5c2
Patrick <patrickwharton1@gmail.com>
2019-02-20 01:40:35 +0000

    SQL beginnings

--- application.py
+++ application.py
@@ -1,7 +1,16 @@
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
-# Configure application
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, redirect, render_template, request, session
+
+# Configure application + database
 app = Flask(__name__)
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Auto reload templates
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
@@ -9,7 +18,7 @@ def index():
 
 @app.route("/register")
 def register():
-    return render_template("error.html", message="TODO", code="404")
+    return render_template("error.html", message="Page not found... I should probably make it...", code="404")
 
 if __name__=="__main__":
         app.run(debug=True)
