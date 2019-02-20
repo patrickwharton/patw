be424eef9fff6a8b3bf59f3a303dff50d0da2877
Patrick <patrickwharton1@gmail.com>
2019-02-20 02:52:55 +0000

    reorganisation

--- application.py
+++ application.py
@@ -17,9 +17,25 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/check", methods=["GET"])
+def check():
+    """Return true if username available, else false, in JSON format"""
+    username = request.args.get("username")
+    if len(username) < 1:
+        return jsonify(False)
+
+    result = None ##db.execute("SELECT username FROM users WHERE username=:username", username=username)
+    if not result:
+        return jsonify(True)
+
+    return jsonify(False)
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return helpers.lol()
+    if request.method == "POST":
+        return helpers.lol()
+    else:
+        return render_template("register.html")
 
 @app.route("/<name>")
 def other(name):
